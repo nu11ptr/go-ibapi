9417bbe17851ee5960d7a1b3eef343b058bdb002
Scott Meeuwsen <3615587+nu11ptr@users.noreply.github.com>
2019-04-06 21:55:35 -0400

    Changed README and readded _obj files

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # go-ibapi
 
+Go bindings for the IB TWS C++ API Client. Note that these are in no way complete, but simply an abstracted wrapper over a portion of the API for our specific needs.
+
 ## Build TWS Shared Library (Linux)
 
-NOTE: Clong IB TWS requires authorization of your github ID from IB
+This step is required before the library can be used.
+
+NOTE: Cloning IB TWS requires authorization of your github ID from IB
 
 ```bash
 git clone git@github.com:InteractiveBrokers/tws-api.git
@@ -10,13 +14,27 @@ cd tws-api
 git checkout 9.74.01
 cd tws-api/source/cppclient/client
 make -f Makefile.linux
+ln -s $PWD <path_to_your_project>/tws-api
 ```
 
-## Build Go Library (Linux)
+# Install
+
+NOTE: Ensure `Build TWS Shared Library` step is done before trying this
+
+NOTE 2: The `_obj` folder contains generated CGo code for Go 1.12.1. If you are using a different version, you may need to clone and regenerate before using.
 
 ```bash
-export CGO_LDFLAGS=<path_to_ib_cpp_api>/libTwsSocketClient.so
-export CGO_CPPFLAGS=-I<path_to_ib_cpp_api>
+export CGO_LDFLAGS=tws-api/libTwsSocketClient.so
+export CGO_CPPFLAGS=-Itws-api
+go get -u github.com/nu11ptr/go-ibapi
+```
+
+## Regenerate CGO Support Files (_obj folder)
+
+This is only required if you are using a Go version other than 1.12.x
+
+```bash
+export CGO_LDFLAGS=tws-api/libTwsSocketClient.so
+export CGO_CPPFLAGS=-Itws-api
 go generate ./...
-go build ./...
 ```
---
+++ _obj/ibapi.cgo2.c
@@ -0,0 +1,305 @@
+
+#line 1 "cgo-builtin-prolog"
+#include <stddef.h> /* for ptrdiff_t and size_t below */
+
+/* Define intgo when compiling with GCC.  */
+typedef ptrdiff_t intgo;
+
+#define GO_CGO_GOSTRING_TYPEDEF
+typedef struct { const char *p; intgo n; } _GoString_;
+typedef struct { char *p; intgo n; intgo c; } _GoBytes_;
+_GoString_ GoString(char *p);
+_GoString_ GoStringN(char *p, int l);
+_GoBytes_ GoBytes(void *p, int n);
+char *CString(_GoString_);
+void *CBytes(_GoBytes_);
+void *_CMalloc(size_t);
+
+__attribute__ ((unused))
+static size_t _GoStringLen(_GoString_ s) { return (size_t)s.n; }
+
+__attribute__ ((unused))
+static const char *_GoStringPtr(_GoString_ s) { return s.p; }
+
+#line 9 "/home/scott/go/src/gitlab.com/apisw/go-ibapi/ibapi.go"
+
+
+
+#include <stdlib.h>
+#include "ibapi.h"
+
+#line 1 "cgo-generated-wrapper"
+
+
+#line 1 "cgo-gcc-prolog"
+/*
+  If x and y are not equal, the type will be invalid
+  (have a negative array count) and an inscrutable error will come
+  out of the compiler and hopefully mention "name".
+*/
+#define __cgo_compile_assert_eq(x, y, name) typedef char name[(x-y)*(x-y)*-2+1];
+
+/* Check at compile time that the sizes we use match our expectations. */
+#define __cgo_size_assert(t, n) __cgo_compile_assert_eq(sizeof(t), n, _cgo_sizeof_##t##_is_not_##n)
+
+__cgo_size_assert(char, 1)
+__cgo_size_assert(short, 2)
+__cgo_size_assert(int, 4)
+typedef long long __cgo_long_long;
+__cgo_size_assert(__cgo_long_long, 8)
+__cgo_size_assert(float, 4)
+__cgo_size_assert(double, 8)
+
+extern char* _cgo_topofstack(void);
+
+/* We use packed structs, but they are always aligned.  */
+/* The pragmas and address-of-packed-member are not recognized as warning groups in clang 3.4.1, so ignore unknown pragmas first. */
+/* remove as part of #27619 (all: drop support for FreeBSD 10). */
+
+#pragma GCC diagnostic ignored "-Wunknown-pragmas"
+#pragma GCC diagnostic ignored "-Wpragmas"
+#pragma GCC diagnostic ignored "-Waddress-of-packed-member"
+
+#include <errno.h>
+#include <string.h>
+
+
+#define CGO_NO_SANITIZE_THREAD
+#define _cgo_tsan_acquire()
+#define _cgo_tsan_release()
+
+
+#define _cgo_msan_write(addr, sz)
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_e05ae29df3db_Cfunc_client_connect(void *v)
+{
+	struct {
+		IBClient* p0;
+		char const* p1;
+		int p2;
+		int p3;
+		_Bool r;
+		char __pad25[7];
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = client_connect(_cgo_a->p0, _cgo_a->p1, _cgo_a->p2, _cgo_a->p3);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_e05ae29df3db_Cfunc_client_disconnect(void *v)
+{
+	struct {
+		IBClient* p0;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	_cgo_tsan_acquire();
+	client_disconnect(_cgo_a->p0);
+	_cgo_tsan_release();
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_e05ae29df3db_Cfunc_client_is_connected(void *v)
+{
+	struct {
+		IBClient* p0;
+		_Bool r;
+		char __pad9[7];
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = client_is_connected(_cgo_a->p0);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_e05ae29df3db_Cfunc_client_process_msg(void *v)
+{
+	struct {
+		IBClient* p0;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	_cgo_tsan_acquire();
+	client_process_msg(_cgo_a->p0);
+	_cgo_tsan_release();
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_e05ae29df3db_Cfunc_contract_currency(void *v)
+{
+	struct {
+		Contract* p0;
+		char const* r;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = (__typeof__(_cgo_a->r)) contract_currency(_cgo_a->p0);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_e05ae29df3db_Cfunc_contract_exchange(void *v)
+{
+	struct {
+		Contract* p0;
+		char const* r;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = (__typeof__(_cgo_a->r)) contract_exchange(_cgo_a->p0);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_e05ae29df3db_Cfunc_contract_month(void *v)
+{
+	struct {
+		Contract* p0;
+		char const* r;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = (__typeof__(_cgo_a->r)) contract_month(_cgo_a->p0);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_e05ae29df3db_Cfunc_contract_sec_type(void *v)
+{
+	struct {
+		Contract* p0;
+		char const* r;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = (__typeof__(_cgo_a->r)) contract_sec_type(_cgo_a->p0);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_e05ae29df3db_Cfunc_contract_symbol(void *v)
+{
+	struct {
+		Contract* p0;
+		char const* r;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = (__typeof__(_cgo_a->r)) contract_symbol(_cgo_a->p0);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_e05ae29df3db_Cfunc_delete_client(void *v)
+{
+	struct {
+		IBClient* p0;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	_cgo_tsan_acquire();
+	delete_client(_cgo_a->p0);
+	_cgo_tsan_release();
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_e05ae29df3db_Cfunc_delete_contract(void *v)
+{
+	struct {
+		Contract* p0;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	_cgo_tsan_acquire();
+	delete_contract(_cgo_a->p0);
+	_cgo_tsan_release();
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_e05ae29df3db_Cfunc_free(void *v)
+{
+	struct {
+		void* p0;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	_cgo_tsan_acquire();
+	free(_cgo_a->p0);
+	_cgo_tsan_release();
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_e05ae29df3db_Cfunc_new_client(void *v)
+{
+	struct {
+		long int p0;
+		long unsigned int p1;
+		IBClient* r;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = (__typeof__(_cgo_a->r)) new_client(_cgo_a->p0, _cgo_a->p1);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
+CGO_NO_SANITIZE_THREAD
+void
+_cgo_e05ae29df3db_Cfunc_new_contract(void *v)
+{
+	struct {
+		char const* p0;
+		char const* p1;
+		char const* p2;
+		char const* p3;
+		char const* p4;
+		Contract* r;
+	} __attribute__((__packed__, __gcc_struct__)) *_cgo_a = v;
+	char *_cgo_stktop = _cgo_topofstack();
+	__typeof__(_cgo_a->r) _cgo_r;
+	_cgo_tsan_acquire();
+	_cgo_r = (__typeof__(_cgo_a->r)) new_contract(_cgo_a->p0, _cgo_a->p1, _cgo_a->p2, _cgo_a->p3, _cgo_a->p4);
+	_cgo_tsan_release();
+	_cgo_a = (void*)((char*)_cgo_a + (_cgo_topofstack() - _cgo_stktop));
+	_cgo_a->r = _cgo_r;
+	_cgo_msan_write(&_cgo_a->r, sizeof(_cgo_a->r));
+}
+
