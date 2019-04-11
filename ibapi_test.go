4d51fe3cdbbe648df3b8e346e14c9b35de6dc776
Scott Meeuwsen <3615587+nu11ptr@users.noreply.github.com>
2019-04-11 08:10:54 -0400

    Added order tests

--- ibapi_test.go
+++ ibapi_test.go
@@ -1,6 +1,7 @@
 package ibapi_test
 
 import (
+	"os"
 	"sync"
 	"testing"
 	"time"
@@ -18,6 +19,36 @@ func TestContract(t *testing.T) {
 	assert.Equal(t, "USD", c.Currency())
 }
 
+func TestLimitOrder(t *testing.T) {
+	o := ibapi.NewOrder(123, "BUY", "LMT", 100.0, 56.0, "DAY")
+	assert.Equal(t, 123, o.ID())
+	assert.Equal(t, "BUY", o.Action())
+	assert.Equal(t, "LMT", o.Type())
+	assert.Equal(t, 100.0, o.Qty())
+	assert.Equal(t, 56.0, o.Price())
+	assert.Equal(t, "DAY", o.TIF())
+}
+
+func TestStopOrder(t *testing.T) {
+	o := ibapi.NewOrder(123, "BUY", "STP", 100.0, 56.0, "DAY")
+	assert.Equal(t, 123, o.ID())
+	assert.Equal(t, "BUY", o.Action())
+	assert.Equal(t, "STP", o.Type())
+	assert.Equal(t, 100.0, o.Qty())
+	assert.Equal(t, 56.0, o.Price())
+	assert.Equal(t, "DAY", o.TIF())
+}
+
+func TestMarketOrder(t *testing.T) {
+	o := ibapi.NewOrder(123, "BUY", "MKT", 100.0, 56.0, "DAY")
+	assert.Equal(t, 123, o.ID())
+	assert.Equal(t, "BUY", o.Action())
+	assert.Equal(t, "MKT", o.Type())
+	assert.Equal(t, 100.0, o.Qty())
+	assert.Equal(t, 0.0, o.Price())
+	assert.Equal(t, "DAY", o.TIF())
+}
+
 type wrapper struct {
 	nextId ibapi.OrderID
 	lock   sync.Mutex
@@ -29,7 +60,20 @@ func (w *wrapper) NextValidId(id ibapi.OrderID) {
 	w.nextId = id
 }
 
+func (w *wrapper) UpdateAccountTime(timeStamp string) {}
+
+func (w *wrapper) Error(id, errorCode int, errorStr string) {}
+
+func (w *wrapper) ConnectionClosed() {}
+
+func (w *wrapper) AccountSummary(reqID int, account, tag, value string) {}
+
+func (w *wrapper) AccountSummaryEnd(reqID int) {}
+
 func TestEClientSocket(t *testing.T) {
+	if _, ok := os.LookupEnv("IB_INTEGRATION"); !ok {
+		t.Skip("'IB_INTEGRATION' not set - skipping")
+	}
 	wrapper := &wrapper{nextId: -1}
 	client := ibapi.NewIBClient(wrapper)
 	assert.True(t, client.Connect("127.0.0.1", 7497, 0))
